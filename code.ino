8927a50f296b16c0610729e4fcf6ede8420ecbe3
Lucas Soares <lsoares267@gmail.com>
2016-11-25 17:48:18 -0200

    Teste de novo código

--- code.ino
+++ code.ino
@@ -6,33 +6,26 @@ void setup()
 {
   posicaoSequencia = 0;
   pinMode(botao,INPUT);
-  //Serial.begin(9600);
+  Serial.begin(9600);
  
   for(int i = 0; i<4; i++)
   {
     pinMode(leds[i],OUTPUT);
   } 
 }
+
+
 void loop() 
 {  
-  if(digitalRead(botao) == HIGH)
-  {
-    posicaoSequencia += 1;
-  }
-
-  if(posicaoSequencia > 2){posicaoSequencia = 0;}
-
-   switch(posicaoSequencia)
-  {
-    case 1:
-    Sequencia(200);
-    break;
-    
-    case 2:
-    Sequencia(100);
-    break;
+  VerificarSequencia();
+  
+  if(posicaoSequencia > 5){
+    Serial.println(posicaoSequencia);
+    posicaoSequencia -= posicaoSequencia;
   }
 }
+
+
 void Sequencia(int delayTime)
 {
   for(int i = 0; i < 4; i++)
@@ -48,4 +41,60 @@ void Sequencia(int delayTime)
   }
 
  delay(delayTime);
+}
+
+
+void SequenciaDois(int SecondDelay)
+{
+  for(int i = 0; i<4; i++)
+  {
+   digitalWrite(leds[i],HIGH);
+   delay(SecondDelay); 
+  }
+
+  for(int i = 0; i<4; i++)
+  {
+    digitalWrite(leds[i],LOW);
+    delay(SecondDelay);
+  }
+}
+
+
+void VerificarSequencia()
+{
+  if(digitalRead(botao) == HIGH)
+  {
+    posicaoSequencia += 1;
+
+    if(posicaoSequencia == 6)
+    {
+      posicaoSequencia = 0;
+    }
+    
+    Serial.println(posicaoSequencia);
+  }
+
+
+   switch(posicaoSequencia)
+  {
+    case 1:
+    Sequencia(200);
+    break;
+    
+    case 2:
+    Sequencia(100);
+    break;
+
+    case 3:
+    SequenciaDois(50);
+    break;
+
+    case 4:
+    SequenciaDois(200);
+    break;
+
+    case 5:
+    SequenciaDois(500);
+    break;
+  }
 }
